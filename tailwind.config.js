e16eb6c105b283bf0e7f1b5889eff11b45c58705
duogbachdev <contact.duogbachdev@gmail.com>
2023-06-29 22:14:10 +0700

    add_slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
       animation: {
         slideDown: 'slideDown .4s ease-in-out',
       },
+      backgroundImage: {
+        'slider-bg': 'url("./img/slider-background.jpeg")'
+      }
     },
     plugins: [],
   },
