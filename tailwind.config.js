1701fc6cdf7bc78080687a1e44271df479f702d2
duogbachdev <contact.duogbachdev@gmail.com>
2023-06-30 17:28:46 +0700

    Add_story_product_cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,15 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
-        }
+        },
+        slideUp: {
+          '0%':{transform:'translate3d(0px,8px,0px)',opacity:0},
+          '100%':{transform:'translate3d(0px,0px,0px)',opacity:1},
+        }  
       },
       animation: {
         slideDown: 'slideDown .4s ease-in-out',
+        slideUp: 'slideUp .4s ease-in',
       },
       backgroundImage: {
         'slider-bg': 'url("./img/slider-background.jpeg")'
